c0a13967395efbacebd5530c37d62a2f9a2d8d3c
Thorgan <amdebarbarin@gmail.com>
2024-11-18 23:12:53 +0100

    Add argument handling with methods and description in -h / --help.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # headers
+
+Here is my first App made in `Rust`, it's just a basic `CLI tool` to make a `GET` request to a `URL` and print the headers contained in the response.
+Of course, you could use `cURL` to do that, but I just wanted to make a somewhat useful simple app with `Rust`,
+instead of making an App that gives the mean of a list of numbers or greets a user, or whatever useless Apps we commonly make to learn a language...
+
+So, pretty simple, you run it indicating the `URL` as argument, and it should give you the headers: simple and stupid.
+
+Here is an example:
+
+![img.png](run_example.png)
+
+Enjoy!
+
+Antoine de Barbarin
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::io;
+use std::ops::Add;
 use std::process::exit;
 use error_chain::error_chain;
-use console::style;
+use clap::{Arg, ArgGroup, ArgMatches, Command};
+use clap::ArgAction::{SetTrue};
+use console::{style, Style};
 use reqwest::header::HeaderMap;
-use reqwest::Response;
+use http::{Method};
 
 error_chain! {
     foreign_links {
@@ -12,18 +15,90 @@ error_chain! {
     }
 }
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: headers <url>");
-        exit(1);
-    }
-    let url = add_proto(&args[1]);
+#[tokio::main]
+async fn main() {
+    let normal_style = Style::new().bold().blue();
+    let author_style = Style::new().bold().cyan();
 
-    let res = make_request(&*url);
+    let matches = Command::new("headers")
+        .about(format!(
+                "{}\n{} {}{}",
+                normal_style.apply_to("A simple CLI tool to retrieve HTTP headers."),
+                normal_style.apply_to("Made in Rust by"),
+                author_style.apply_to("Antoine de Barbarin"),
+                normal_style.apply_to(".")
+                ))
+        .arg(
+            // Only required argument: the URL to send the request to
+            Arg::new("url")
+            .value_name("URL")
+            .required(true)
+            .help("The URL to get the headers from"))
+        .arg(Arg::new("get")
+                .short('G')
+                .long("get")
+                .help("Use method GET")
+                .action(SetTrue)
+                .required(false))
+        .arg(Arg::new("post")
+                .short('S')
+                .long("post")
+                .help("Use method POST")
+                .action(SetTrue)
+                .required(false))
+        .arg(Arg::new("put")
+                .short('U')
+                .long("put")
+                .help("Use method PUT")
+                .action(SetTrue)
+                .required(false))
+        .arg(Arg::new("delete")
+                .short('D')
+                .long("delete")
+                .help("Use method DELETE")
+                .action(SetTrue)
+                .required(false))
+        .arg(Arg::new("patch")
+                .short('A')
+                .long("patch")
+                .help("Use method PATCH")
+                .action(SetTrue)
+                .required(false))
+        .arg(Arg::new("options")
+                .short('O')
+                .long("options")
+                .help("Use method OPTIONS")
+                .action(SetTrue)
+                .required(false))
+        .arg(Arg::new("trace")
+                .short('T')
+                .long("trace")
+                .help("Use method TRACE")
+                .action(SetTrue)
+                .required(false))
+        .arg(Arg::new("head")
+                .short('H')
+                .long("head")
+                .help("Use method HEAD")
+                .action(SetTrue)
+                .required(false))
+        .group(ArgGroup::new("method")
+                .args(&["get", "post", "put", "delete", "patch", "options", "trace", "head"])
+                .required(false))
+        .get_matches();
+
+    // Get the url argument
+    let url = check_proto(matches.get_one::<String>("url").unwrap());
+
+    // Get the method argument
+    let method = get_method(&matches).unwrap();
+
+    let res = make_request(&url, &method).await;
     match res {
         Ok(headers) => {
-            let result = print_headers(&*url, &headers.headers());
+            println!();
+            println!("{} {} {}\n", style(String::from("-> Request")).bold().blue(), style(method.to_string()).bold().cyan(), style(url).bold().yellow());
+            let result = print_headers(&headers);
             if let Err(_) = result { exit_error() }
         }
         _err => { exit_error() }
@@ -34,19 +109,26 @@ fn exit_error() {
     println!("Something went wrong..."); exit(1)
 }
 
-fn add_proto(url: &String) -> String {
+fn check_proto(url: &String) -> String {
     if !url.starts_with("http://") & !url.starts_with("https://") {
-        "https://".to_string() + url
+        String::from("https://").add(url)
     } else {
-        url.to_string()
+        String::from(url)
     }
 }
 
-fn print_headers(url: &str, headers: &HeaderMap) -> io::Result<()> {
-    let title = String::from("Request to ") + url;
-    println!();
-    println!("{}\n", style(title).bold().yellow());
+fn get_method(matches: &ArgMatches) -> Option<Method> {
+    Some(matches.get_flag("get").then_some(Method::GET)
+        .or_else(|| matches.get_flag("post").then_some(Method::POST))
+        .or_else(|| matches.get_flag("put").then_some(Method::PUT))
+        .or_else(|| matches.get_flag("delete").then_some(Method::DELETE))
+        .or_else(|| matches.get_flag("patch").then_some(Method::PATCH))
+        .or_else(|| matches.get_flag("options").then_some(Method::OPTIONS))
+        .or_else(|| matches.get_flag("trace").then_some(Method::TRACE))
+        .unwrap_or(Method::HEAD))
+}
 
+fn print_headers(headers: &HeaderMap) -> io::Result<()> {
     let mut max_header_name_len = 0;
     for (k, _v) in headers.iter() {
         if k.as_str().len() > max_header_name_len {
@@ -65,11 +147,14 @@ fn print_headers(url: &str, headers: &HeaderMap) -> io::Result<()> {
         print!("{:1$}", style(header).bold().green(), max_header_name_len);
         println!("{}", style(value).blue());
     }
+
+    println!();
     Ok(())
 }
 
-#[tokio::main]
-async fn make_request(url: &str) -> Result<Response> {
-    let res = reqwest::get(url).await?;
-    Ok(res)
+async fn make_request(url: &str, method: &Method) -> Result<HeaderMap> {
+    let client = reqwest::Client::new();
+    let res = client.request(method.clone(), url).send().await?;
+    let headers = res.headers();
+    Ok(headers.clone())
 }
